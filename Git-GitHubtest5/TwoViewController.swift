e8b5c0a9330dc62b3af4d8f58ab2610768f19a16
赵强 <568423952@qq.com>
2021-07-21 14:40:32 +0800

    你好你好呢

---
+++ Git-GitHubtest5/TwoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TwoViewController.swift
+//  Git-GitHubtest5
+//
+//  Created by ZhaoQ on 2021/7/21.
+//
+
+import UIKit
+
+class TwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
